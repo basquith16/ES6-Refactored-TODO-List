b0acc3ca7e1239d53757bdebbaf0612cb8f59af1
Brian M Asquith <basquithcpt@gmail.com>
2015-09-30 17:35:02 -0400

    finished

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+$('form').submit(function() {
+  if ($('input').val() !== '') {
+    const value = $('input').val();
+    $('ul').append('<li>' + value + '<a href=>completed</a></li>');
+  };
+  $('input').val('');
+  return false;
+});
+$(document).on('click', 'a', function(e) {
+  e.preventDefault();
+  $(this).parent().wrap("<strike>");
+  $(this).fadeOut("slow");
+});
